3c9d63e207f4043957ce4127c299970af112c48d
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2025-11-12 16:44:11 -0300

    Muda types_and_params para incluir bit_type.h

--- gen_test/types_and_params.h
+++ gen_test/types_and_params.h
@@ -2,10 +2,10 @@
 #define _TYPES_AND_PARAMS_H_
 
 #include "ap_fixed.h"
-#include "ap_int.h"
 
-typedef ap_uint<1> bit_t;
-typedef ap_fixed<16, 8> input_t;
+#include "neuro_hls_functions/bit_type.h"
+
 typedef ap_fixed<16, 8> potential_t;
+typedef ap_fixed<16, 8> input_t;
 
 #endif
