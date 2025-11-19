4808622fe71bf7c5671d4eb413e09e84891b1bc1
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2025-11-19 16:13:08 -0300

    TestbenchEditor funcionando

--- export-nir-from-snnTorch-main/TestbenchEditor.py
+++ export-nir-from-snnTorch-main/TestbenchEditor.py
@@ -10,17 +10,42 @@ class TestbenchEditor:
         self._different_sample_per_step = different_sample_per_step
         self._folder_path = folder_path
 
-    def _get_decl_input_data(self):
+    def _get_decl_input_data_code(self):
 
         input_dims = ""
 
-        if self.different_sample_per_step:
+        if self._different_sample_per_step:
             input_dims += "[STEP_COUNT]"
 
         for i in range(1, self._input_component_count + 1):
             input_dims += f"[DIM_{i}]"
 
         return f"input_t input_data[BATCH_SIZE]{input_dims};"
+    
+    def _get_feed_snn_code(self):
+
+        step_dimension_string = "[s]" if self._different_sample_per_step else ""
+        return f"snn_mnist_hls(input_data[b]{step_dimension_string}, output);"
+    
+    def _get_read_batch_code(self):
+
+        input_file_read = "input_file >> input_data[b]"
+        read_batch = IndentationMaker(3, first_line_should_use_indentation=False)
+
+        if self._different_sample_per_step:
+            input_file_read += "[s]"
+            
+            read_batch.append_line(f"for (int s = 0; s < STEP_COUNT; s++)")
+            read_batch.add_scope()
+        
+        for i in range(1, self._input_component_count + 1):
+            input_file_read += f"[d{i}]"
+
+            read_batch.append_line(f"for (int d{i} = 0; d{i} < DIM_{i}; d{i}++)")
+            read_batch.add_scope()
+
+        read_batch.append_line(input_file_read + ";")
+        return read_batch.get_text()
 
     def finish_main(self):
 
@@ -30,43 +55,20 @@ class TestbenchEditor:
             tb_cpp = f.read()
 
         # Declaration input_data
-
-        tb_cpp = tb_cpp.replace("//<decl_input_data>", self._get_decl_input_data())
+        tb_cpp = tb_cpp.replace("//<decl_input_data>", self._get_decl_input_data_code())
 
         # Feed data to the SNN
-
-        step_dimension_string = "[s]" if self.different_sample_per_step else ""
-        feed_data_snn = f"snn_mnist_hls(input_data[b]{step_dimension_string}, output);"
-        tb_cpp = tb_cpp.replace("//<feed_data_snn>", feed_data_snn)
+        tb_cpp = tb_cpp.replace("//<feed_data_snn>", self._get_feed_snn_code())
 
         # Read data from input file
-
-        input_file_read = "input_file >> input_data[b]"
-        read_batch = IndentationMaker(3, first_line_should_use_indentation=False)
-
-        idx = 1
-
-        for dim in self.constants.keys():
-
-            if dim == "OUTPUT_SIZE":
-                continue
-
-            if dim == "STEP_COUNT":
-                
-                if not self.different_sample_per_step:
-                    continue
-
-                read_batch.append_line(f"for (int s = 0; s < {dim}; s++)")
-                input_file_read += "[s]"
-            else:
-                read_batch.append_line(f"for (int d{idx} = 0; d{idx} < {dim}; d{idx}++)")
-                input_file_read += f"[d{idx}]"
-                idx += 1
-
-            read_batch.add_scope()
-
-        read_batch.append_line(input_file_read + ";")
-        tb_cpp = tb_cpp.replace("//<read_batch>", read_batch.get_text())
+        tb_cpp = tb_cpp.replace("//<read_batch>", self._get_read_batch_code())
 
         with open(tb_name, "w", encoding="utf-8") as f:
             f.write(tb_cpp)
+
+def test_testbench_editor():
+
+    tb_editor = TestbenchEditor(1, 10, 10, False, "gen_test")
+    tb_editor.finish_main()
+
+test_testbench_editor()
