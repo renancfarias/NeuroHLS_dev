345d3870160b3ec133109d7f65df31afa1bdc684
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2025-12-03 15:21:58 -0300

    Add NeuroHls.py

---
+++ export-nir-from-snnTorch-main/NeuroHls.py
@@ -0,0 +1,43 @@
+import subprocess
+
+class NeuroHls:
+
+    def __init__(self, folder_path: str):
+
+        self._folder_path = folder_path
+
+        self._has_parsed_nir = False
+        self._has_created_testbench = False
+
+    def parse_nir(self, nir):
+
+        # Obter esses valores com o NIR
+        self._input_shape = (784,)
+        self._output_size = 10
+
+        self._has_parsed_nir = True
+
+    def create_test_dataset(self):
+        pass
+
+    def define_testbench_parameters(self):
+        pass
+
+    def create_testbench(self, step_count: int, different_sample_per_step: bool):
+
+        if not self._has_parsed_nir:
+            print("ERROR: The network architecture must be defined before creating the testbench files.")
+            return
+        
+        self._has_created_testbench = True
+
+    def run_csim(self):
+
+        if not self._has_created_testbench:
+            print("ERROR: The testbench files must be created before running the C-Simulation.")
+            return
+        
+        subprocess.run(["vitis_hls", "-f", "run.tcl"], cwd = self._folder_path)
+
+    def run_csynth(self):
+        pass
