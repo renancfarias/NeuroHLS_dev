33bc455b5f1475a609ecb924adba27ddcf013c2c
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2025-12-09 16:24:50 -0300

    Melhora ModelConfig

--- NeuroHls/ModelConfig.py
+++ NeuroHls/ModelConfig.py
@@ -1,34 +1,35 @@
-from types import SimpleNamespace
+from typing import List
+from .FileGenUtils import get_closest_divisor
 
 class LayerConfig:
 
     def __init__(self):
 
-        self.accum_unroll_factor = 1
-        self.fire_unroll_factor = 1
+        self._accum_unroll_factor = 1
+        self._fire_unroll_factor = 1
 
-        self.quant_total_bits = 16
-        self.quant_int_bits = 8
+        self._potentials_total_bits = 16
+        self._potentials_int_bits = 8
 
     def __str__(self):
-        s = "  Unroll Factors:\n"
-        s += f"    - Accum: {self.accum_unroll_factor}\n"
-        s += f"    - Fire: {self.fire_unroll_factor}\n"
+        s = "\tUnroll Factors:\n"
+        s += f"\t\t- Accum: {self._accum_unroll_factor}\n"
+        s += f"\t\t- Fire: {self._fire_unroll_factor}\n"
 
-        s += f"  Quantization (ap_fixed<{self.quant_total_bits}, {self.quant_int_bits}>):\n"
-        s += f"    - Total bits: {self.quant_total_bits}\n"
-        s += f"    - Integer bits: {self.quant_int_bits}\n"
-        s += f"    - Fractional bits: {self.quant_total_bits - self.quant_int_bits}\n"
+        s += f"\tQuantization (ap_fixed<{self._potentials_total_bits}, {self._potentials_int_bits}>):\n"
+        s += f"\t\t- Total bits: {self._potentials_total_bits}\n"
+        s += f"\t\t- Integer bits: {self._potentials_int_bits}\n"
+        s += f"\t\t- Fractional bits: {self._potentials_total_bits - self._potentials_int_bits}\n"
 
         return s
     
     def set_potential_quantization(self, total_bits, int_bits):
-        self.quant_total_bits = total_bits
-        self.quant_int_bits = int_bits
+        self._potentials_total_bits = total_bits
+        self._potentials_int_bits = min(int_bits, total_bits)
 
     def set_unroll_factors(self, accum_unroll_factor, fire_unroll_factor):
-        self.accum_unroll_factor = accum_unroll_factor
-        self.fire_unroll_factor = fire_unroll_factor
+        self._accum_unroll_factor = accum_unroll_factor
+        self._fire_unroll_factor = fire_unroll_factor
 
 class DenseLayerConfig(LayerConfig):
 
@@ -39,57 +40,50 @@ class DenseLayerConfig(LayerConfig):
         self.n_neurons = n_neurons
 
     def __str__(self):
-        s = "-" * 30 + "\n"
-        s += f"Dense ({self.n_inputs}, {self.n_neurons})\n\n"
+        s = f"Dense ({self.n_inputs}, {self.n_neurons})\n"
+        s += "-" * 30 + "\n\n"
         s += super().__str__()
 
         return s
+    
+    def set_unroll_factors(self, accum_unroll_factor, fire_unroll_factor, verbose=True):
 
-class ModelConfigObjectCreator:
+        correct_accum_unroll_factor = get_closest_divisor(self.n_inputs, accum_unroll_factor)
+        correct_fire_unroll_factor = get_closest_divisor(self.n_neurons, fire_unroll_factor)
+
+        super().set_unroll_factors(correct_accum_unroll_factor, correct_fire_unroll_factor)
+
+        if not verbose:
+            return
+
+        if correct_accum_unroll_factor != accum_unroll_factor:
+            print(f"The accumulation unroll factor must be a divisor of the number of inputs ({self.n_inputs}). Used value: {correct_accum_unroll_factor}")
+        else:
+            print(f"Used accumulation unroll factor: {correct_accum_unroll_factor}")
+
+        if correct_fire_unroll_factor != fire_unroll_factor:
+            print(f"The fire unroll factor must be a divisor of the number of neurons ({self.n_neurons}). Used value: {correct_fire_unroll_factor}")
+        else:
+            print(f"Used fire unroll factor: {correct_fire_unroll_factor}")
+
+class ModelConfig:
     
     def __init__(self):
-        self.obj = SimpleNamespace()
-        self.layer_count = 1
 
-    # def __str__(self):
-    #     s = ""
+        self.layers: List[LayerConfig] = []
 
-    #     for layer in self.layers:
-    #         s += layer.__str__()
+    def __str__(self):
+        s = ""
+
+        for idx, layer in enumerate(self.layers):
+
+            if idx > 0:
+                s += "\n"
+            s += 30 * "-" + "\n"
+            s += f"Layer {idx+1}: {layer.__str__()}"
         
-    #     return s
+        return s
     
     def add_layer(self, layer: LayerConfig):
 
-        if isinstance(layer, DenseLayerConfig):
-            layer_name = f"layer_{self.layer_count}_DENSE_{layer.n_inputs}_{layer.n_neurons}"
-            setattr(self.obj, layer_name, layer)
-
-        self.layer_count += 1
-
-    def get_object(self):
-        return self.obj
-    
-# def test_model_config():
-
-#     # Feito a partir da leitura do NIR
-
-#     layer_1 = DenseLayerConfig(784, 128)
-#     layer_2 = DenseLayerConfig(128, 10)
-
-#     model = ModelConfigObjectCreator()
-#     model.add_layer(layer_1)
-#     model.add_layer(layer_2)
-
-#     print(model)
-
-#     # User pode definir as configurações
-
-#     model.layers[0].set_unroll_factors(10, 10)
-#     model.layers[1].set_potential_quantization(32, 4)
-#     print(model.layers[0])
-#     print(model.layers[1])
-
-    
-
-# test_model_config()
+        self.layers.append(layer)
