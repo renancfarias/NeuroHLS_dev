477703ee5da3f5ce9526eb98d1bb17125a1f6ad1
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2025-12-12 17:19:08 -0300

    Permite que camadas tenham quantizações diferentes

--- test_implementation_manager.py
+++ test_implementation_manager.py
@@ -9,18 +9,15 @@ def test_dense():
     model_config.add_layer(DenseLayerConfig(784, 128))
     model_config.add_layer(DenseLayerConfig(128, 10))
 
+    model_config.set_input_quantization(10, 2)
+    model_config.set_default_potential_quantization(25, 6)
+
+    model_config.layers[0].set_potential_quantization(43, 10)
+
     print(model_config)
 
     impl_manager = ImplementationManager("z_test")
 
     impl_manager.create_files_from_config(model_config)
 
-    # test_cpp = ImplementationManager(784)
-
-    # test_cpp._dense(784, 128, "potential_t")
-    # test_cpp._dense(128, 10, "potential_t", is_output_layer=True)
-    
-    # test_cpp.generate_files("gen_test")
-
-# test_conv()
 test_dense()
