0c731af0bef51efbd553fdb162ab515ae7efe195
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2026-01-21 17:09:01 -0300

    Gera C++ de teste no teste_ciclos

--- teste_ciclos_nir.py
+++ teste_ciclos_nir.py
@@ -8,6 +8,12 @@ from typing import Dict, List, Tuple, Any, Iterable
 from collections import defaultdict
 from collections import deque
 
+def get_bracket_str(arr):
+    if arr is not None:
+        return ''.join(f'[{x}]' for x in arr)
+
+    return ""
+
 def teste(nir_file: str):
 
     print("\n" + "-" * 60)
@@ -27,9 +33,6 @@ def teste(nir_file: str):
         graph[e[0]].append(e[1])
         dependencies[e[1]].append(e[0])
 
-    print(f"graph: {graph}")
-    print(f"dep: {dependencies}\n\n")
-
     visited = set()
     queue = deque()
 
@@ -38,25 +41,48 @@ def teste(nir_file: str):
         cur = queue.popleft()
         visited.add(cur)
 
-        print(f"{cur}:")
+        node_info = nodes[cur]
+        func_name = type(node_info).__name__
 
-        print("  - gets: ", end="")
-        for dep in dependencies[cur]:
-            if dep not in visited:
-                print(f"{dep} (recurrent), ", end="")
-            else:
-                print(f"{dep} (ready), ", end="")
-        print()
+        output_bracket = get_bracket_str(node_info.output_type['output'])
 
-        print("  - sends: ", end="")
         for node in graph[cur]:
             if node not in visited:
                 queue.append(node)
-                print(f"{node}, ", end="")
-            else:
-                print(f"{node} (back), ", end="")
-            
-        print("\n\n")
+
+        if func_name == "Input":
+            print(f"snn_hls(input{output_bracket})")
+            continue
+
+        ### Should declare potential (not recurrent)
+
+        should_decl_potential = True
+        for node in graph[cur]:
+            if node in visited:
+                should_decl_potential = False
+
+        if should_decl_potential:
+            print(f"    {cur}{output_bracket} = {{}}")
+
+        ### Declaring dependencies (if recurrent)
+
+        for dep in dependencies[cur]:
+            if dep not in visited:
+                print(f"    {dep}{get_bracket_str(nodes[dep].input_type['input'])} = {{}} // recurrent")
+
+        ### Adding dependecies
+
+        if len(dependencies[cur]) == 0:
+            continue
+
+        if len(dependencies[cur]) == 1: # Sem recorrencia
+            print(f"    {func_name}({dependencies[cur][0]}, {cur});")
+        else:
+            for dep in dependencies[cur][1:]:
+                print(f"    merge({dependencies[cur][0]}, {dep});")
+            print(f"    {func_name}({dependencies[cur][0]}, {cur});")
+        print()
 
 # teste("z_nir_examples/lif_norse.nir")
+# teste("z_nir_examples/cnn_sinabs.nir")
 teste("z_nir_examples/braille_noDelay_bias_zero.nir")
