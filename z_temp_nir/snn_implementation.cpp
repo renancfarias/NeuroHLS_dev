eae6873fc419090b1b997f7d1567140b981b3eb3
Fernando Pozzer <fernando.p.pozzer@gmail.com>
2026-02-03 16:31:08 -0300

    Cria arquivo .cpp do modelo e .h dos parâmetros dos neurônios

---
+++ z_temp_nir/snn_implementation.cpp
@@ -0,0 +1,42 @@
+void snn_to_hls(input_t input[12], bit_t output[7])
+{
+//--------------------------------------------------
+// implementation of 'fc1' layer
+//--------------------------------------------------
+
+	type_t layer_1[38] = {};
+	Affine(input, layer_1);
+
+//--------------------------------------------------
+// implementation of 'merge_1' layer
+//--------------------------------------------------
+
+	type_t layer_2_rec[38] = {};
+	Merge(layer_1, layer_2_rec);
+
+//--------------------------------------------------
+// implementation of 'lif1.lif' layer
+//--------------------------------------------------
+
+	bit_t layer_3[38] = {};
+	CubaLIF(layer_1, layer_3, tau_syn_3, tau_mem_3, r_3, v_leak_3, v_threshold_3, v_reset_3, w_in_3);
+
+//--------------------------------------------------
+// implementation of 'lif1.w_rec' layer
+//--------------------------------------------------
+
+	Affine(layer_3, layer_2_rec);
+
+//--------------------------------------------------
+// implementation of 'fc2' layer
+//--------------------------------------------------
+
+	type_t layer_4[7] = {};
+	Affine(layer_3, layer_4);
+
+//--------------------------------------------------
+// implementation of 'lif2' layer
+//--------------------------------------------------
+
+	CubaLIF(layer_4, output, tau_syn_5, tau_mem_5, r_5, v_leak_5, v_threshold_5, v_reset_5, w_in_5);
+}
